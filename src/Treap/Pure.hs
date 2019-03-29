ae1776dd9883ae5fa2967c22ef5ce381e7506c22
Dmitry Kovanikov <kovanikov@gmail.com>
2019-03-29 23:22:36 +0800

    Implement basic update functions

--- src/Treap/Pure.hs
+++ src/Treap/Pure.hs
@@ -19,8 +19,13 @@ module Treap.Pure
          -- * Core functions
        , split
        , merge
+
+         -- * Interface functions
+       , insert
+       , delete
        ) where
 
+import Data.Foldable (foldl')
 import GHC.Exts (IsList (..))
 import GHC.Generics (Generic)
 
@@ -34,14 +39,20 @@ data Treap k p a
     | Empty
     deriving (Show, Read, Generic, Functor, Foldable, Traversable)
 
-instance IsList (Treap k p a) where
+{- | Typeclass that allows to create 'Treap' from the list of triples. If all
+priorities are random then the expected performance of the @fromList@ function
+is \( O(n\ \log \ n)\).
+-}
+instance (Ord k, Ord p) => IsList (Treap k p a) where
     type Item (Treap k p a) = (k, p, a)
 
     fromList :: [(k, p, a)] -> Treap k p a
-    fromList = error "Not implemented!"
+    fromList = foldl' (\t (k, p, a) -> insert k p a t) Empty
 
+    -- TODO: make more efficient
     toList :: Treap k p a -> [(k, p, a)]
-    toList = error "Not implemented!"
+    toList Empty            = []
+    toList (Node k p a l r) = toList l ++ (k, p, a) : toList r
 
 ----------------------------------------------------------------------------
 -- Smart constructors
@@ -95,3 +106,35 @@ merge l Empty = l
 merge l@(Node k1 p1 a1 l1 r1) r@(Node k2 p2 a2 l2 r2)
     | p1 > p2   = Node k1 p1 a1 l1 (merge r1 r)
     | otherwise = Node k2 p2 a2 (merge l l2) r2
+
+----------------------------------------------------------------------------
+-- Core functions
+----------------------------------------------------------------------------
+
+-- | \( O(d) \). Insert a value into 'Treap' by given key and priority.
+insert :: forall k p a . (Ord k, Ord p) => k -> p -> a -> Treap k p a -> Treap k p a
+insert k p a = go
+  where
+    go :: Treap k p a -> Treap k p a
+    go Empty = one k p a
+    go t@(Node tk tp ta l r)
+      | p <= tp = case compare k tk of
+          EQ -> t
+          LT -> Node tk tp ta (go l) r
+          GT -> Node tk tp ta l (go r)
+      | otherwise =
+          let (!newL, !newR) = split k t
+          in Node k p a newL newR
+
+{- | \( O(d) \). Delete 'Treap' node that contains given key. If there is no
+such key, 'Treap' remains unchanged.
+-}
+delete :: forall k p a . (Ord k, Ord p) => k -> Treap k p a -> Treap k p a
+delete k = go
+  where
+    go :: Treap k p a -> Treap k p a
+    go Empty = Empty
+    go (Node tk tp ta l r) = case compare k tk of
+        EQ -> merge l r
+        LT -> Node tk tp ta (go l) r
+        GT -> Node tk tp ta l (go r)
