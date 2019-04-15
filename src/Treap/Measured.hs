273b6b4f7673ef2fab4b4cf86a1079d71bde6c44
Dmitrii Kovanikov <dmitrii@holmusk.com>
2019-04-15 15:23:40 +0800

    [#10] Add more instances for Measured
    Resolves #10

--- src/Treap/Measured.hs
+++ src/Treap/Measured.hs
@@ -8,7 +8,9 @@ module Treap.Measured
        ) where
 
 import Data.Functor.Identity (Identity (..))
-import Data.Monoid (Product (..), Sum (..))
+import Data.Monoid (All (..), Any (..), Dual (..), Endo (..), First (..), Last (..), Product (..),
+                    Sum (..))
+import Data.Semigroup (Max (..), Min (..))
 
 
 class Monoid m => Measured m a where
@@ -19,15 +21,50 @@ instance Monoid a => Measured (Identity a) a where
     measure = Identity
     {-# INLINE measure #-}
 
--- -- | Measure everything as @()@.
--- instance Measured () a where
---     measure _ = ()
---     {-# INLINE measure #-}
+-- | Measure every 'Monoid' as its dual.
+instance Monoid a => Measured (Dual a) a where
+    measure = Dual
+    {-# INLINE measure #-}
 
+-- | Measure every endomorphic function with compostion.
+instance Measured (Endo a) (a -> a) where
+    measure = Endo
+    {-# INLINE measure #-}
+
+-- | Measure every numeric value with addition.
 instance Num a => Measured (Sum a) a where
     measure = Sum
     {-# INLINE measure #-}
 
+-- | Measure every numeric value with multiplication.
 instance Num a => Measured (Product a) a where
     measure = Product
     {-# INLINE measure #-}
+
+-- | Measure every comparable value with minimum.
+instance (Ord a, Bounded a) => Measured (Min a) a where
+    measure = Min
+    {-# INLINE measure #-}
+
+-- | Measure every comparable value with maximum.
+instance (Ord a, Bounded a) => Measured (Max a) a where
+    measure = Max
+    {-# INLINE measure #-}
+
+-- | Measure every value as the 'First' monoid.
+instance Measured (First a) a where
+    measure = First . Just
+    {-# INLINE measure #-}
+
+-- | Measure every value as the 'Last' monoid.
+instance Measured (Last a) a where
+    measure = Last . Just
+    {-# INLINE measure #-}
+
+-- | Measure boolean value with '&&'.
+instance Measured All Bool where
+    measure = All
+
+-- | Measure boolean value with '||'.
+instance Measured Any Bool where
+    measure = Any
